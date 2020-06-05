1d6ebadc6c9820bea9cc2c7c401d815f00d2a4d3
Andreja Kogovsek <andreja.kogovsek1@gmail.com>
2020-06-05 08:52:46 +0100

    Set up ESLint

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "plugin:@typescript-eslint/recommended",
+  ],
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly",
+  },
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 11,
+    sourceType: "module",
+    project: "./tsconfig.json",
+  },
+  plugins: ["react", "react-hooks", "@typescript-eslint"],
+  settings: {
+    react: { version: "detect" },
+  },
+};
